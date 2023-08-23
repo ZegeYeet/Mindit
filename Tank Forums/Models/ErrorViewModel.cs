c767e9bf01d4c91f588238352ec8ed5908b2a839
Zegetable <38813761+Zegetable@users.noreply.github.com>
2023-08-23 17:15:22 -0500

    refactoring to new mindit general forum project

---
+++ Tank Forums/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mindit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Tank Forums/wwwroot/js/site.js
@@ -0,0 +1,101 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+async function ClickVoteButton(postId, voteButton, voteCountText)
+{
+    if (ForumPostChangeVote(postId, voteButton.value, voteCountText)) {
+        ToggleVoteButtonIcon(voteButton);
+    }
+    
+}
+
+function ToggleVoteButtonIcon(element)
+{
+
+    if (element.value == "upvoteEmpty") {
+        element.style.backgroundImage = "url('Images/upvoteIconFull.png')";
+        element.value = "upvoteFull";
+    }
+    else if (element.value == "upvoteFull") {
+        element.style.backgroundImage = "url('Images/upvoteIconEmpty.png')";
+        element.value = "upvoteEmpty";
+    }
+    else if (element.value == "downvoteEmpty") {
+        element.style.backgroundImage = "url('Images/downvoteIconFull.png')";
+        element.value = "downvoteFull";
+    }
+    else if (element.value == "downvoteFull") {
+        element.style.backgroundImage = "url('Images/downvoteIconEmpty.png')";
+        element.value = "downvoteEmpty";
+    }
+}
+
+function ForumPostRefreshVotes(postId, voteCountText)
+{
+
+
+    $.ajax({
+        url: '/ForumPosts/GetVoteCount/',
+        type: 'GET',
+        data: {'postID': postId},
+        dataType: 'json',
+        success: function (data) {
+            voteCountText.innerHTML = data.voteCount;
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
+
+async function ForumPostChangeVote(postId, newVoteValue, voteCountText) {
+
+
+    $.ajax({
+        url: '/ForumPosts/ChangeVoteCount/',
+        type: 'POST',
+        data: { 'postID': postId, 'voteValue': newVoteValue },
+        dataType: 'json',
+        success: function (data) {
+            ForumPostRefreshVotes(postId, voteCountText);
+            SetIconsAfterVote(postId, data.voteStyle);
+            return true;
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+
+    return false;
+}
+
+function SetIconsAfterVote(postId, voteValue) {
+
+    var upvoteButton = document.getElementById("upvoteButton " + postId);
+    var downvoteButton = document.getElementById("downvoteButton " + postId);
+
+    if (voteValue == "upvoteFull") {
+        upvoteButton.style.backgroundImage = "url('Images/upvoteIconFull.png')";
+        upvoteButton.value = "upvoteFull";
+        downvoteButton.style.backgroundImage = "url('Images/downvoteIconEmpty.png')";
+        downvoteButton.value = "downvoteEmpty";
+    }
+    else if (voteValue == "downvoteFull") {
+        upvoteButton.style.backgroundImage = "url('Images/upvoteIconEmpty.png')";
+        upvoteButton.value = "upvoteEmpty";
+        downvoteButton.style.backgroundImage = "url('Images/downvoteIconFull.png')";
+        downvoteButton.value = "downvoteFull";
+    }
+    else { //neutral or no vote cases
+        upvoteButton.style.backgroundImage = "url('Images/upvoteIconEmpty.png')";
+        upvoteButton.value = "upvoteEmpty";
+        downvoteButton.style.backgroundImage = "url('Images/downvoteIconEmpty.png')";
+        downvoteButton.value = "downvoteEmpty";
+    }
+
+}
+
